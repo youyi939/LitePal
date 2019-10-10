8e6469ea1c5b9988717f8eaafc0395a84a7a5e16
youyi <1291174818@qq.com>
2019-10-10 16:37:04 +0800

    LitePal增删改查

---
+++ app/src/main/java/com/example/litepal/Book.java
@@ -0,0 +1,42 @@
+package com.example.litepal;
+
+import org.litepal.crud.LitePalSupport;
+
+public class Book extends LitePalSupport {
+    private int id;
+    private String author;
+    private String name;
+    private double price;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+}
---
+++ app/src/main/java/com/example/litepal/Main2Activity.java
@@ -0,0 +1,76 @@
+package com.example.litepal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import org.litepal.LitePal;
+
+public class Main2Activity extends AppCompatActivity {
+
+    private Button create;
+    private Button add;
+    private Button delete;
+    private Button find;
+    private Button updata;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        create = findViewById(R.id.create);
+        add = findViewById(R.id.add);
+        delete = findViewById(R.id.delete);
+        find = findViewById(R.id.find);
+        updata = findViewById(R.id.updata);
+
+
+        create.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                LitePal.getDatabase();
+                Toast.makeText(Main2Activity.this,"数据库创建成功",Toast.LENGTH_LONG).show();
+            }
+        });
+
+
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Book book = new Book();
+                book.setName("三国");
+                book.setAuthor("吴承恩");
+                book.setPrice(16.1);
+                book.save();
+                Toast.makeText(Main2Activity.this,"添加数据成功",Toast.LENGTH_LONG).show();
+            }
+        });
+
+        updata.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Book book = new Book();
+                book.setName("水壶");
+                book.updateAll("name = ?","三国");
+                Toast.makeText(Main2Activity.this,"修改数据成功",Toast.LENGTH_LONG).show();
+            }
+        });
+
+
+        delete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+
+
+
+    }
+}
